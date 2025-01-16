75644572a3ae5fe7800cdd90e327ac044ce3b318
maureen-james <manjax97@gmail.com>
2025-01-16 11:15:07 +0300

    Add navbar

--- my-portifolio/src/app/page.js
+++ my-portifolio/src/app/page.js
@@ -1,34 +1,91 @@
-"use client";
-
-import React from 'react';
-import NavigationBar from '../@core/components/Navigation';
-import { Container } from 'react-bootstrap';
-import About from '../@core/components/About';
-import Experience from '../@core/components/Experience';
-import Projects from '../@core/components/Projects';
-import Contact from '@/@core/components/Contact';
-import Footer from '@/@core/components/Footer';
+import React from "react";
+import NavigationBar from "../@core/components/Navigation";
+import { Container } from "react-bootstrap";
+import About from "../@core/components/About";
+import Experience from "../@core/components/Experience";
+import Projects from "../@core/components/Projects";
+import Contact from "@/@core/components/Contact";
+import Footer from "@/@core/components/Footer";
 
 const Page = () => {
   return (
     <>
       {/* Navbar */}
-      <Container>
       <NavigationBar />
-      
+
       {/* Main Content */}
-        <div className="intro">
-          <div className="title">
-            <h3>I'm Maureen Mwangi</h3>
-            <p>Software Developer and Web Designer</p>
+      <Container fluid style={{ padding: 0, margin: 0 }}>
+        {/* Intro Section */}
+        <div
+          className="intro"
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            alignItems: "center",
+            justifyContent: "center", // Center the content horizontally
+            backgroundColor: "#f0f8ff", // Light blue background
+            padding: "30px 15px", // Reduced padding
+            gap: "20px", // Add spacing between elements
+          }}
+        >
+          <div className="title" style={{ textAlign: "center", flex: 1 }}> 
+            <h3 style={{ marginBottom: "10px", fontWeight: "bold", fontSize: "1.8rem" }}>
+              I'm Maureen Mwangi
+            </h3>
+            <p style={{ fontSize: "1rem", lineHeight: "1.5" }}>
+              Software Developer and Web Designer
+            </p>
           </div>
-          <img src="/static/images/me1.png" alt="Maureen Mwangi" className="left" />
+          <img
+            src="/static/images/me1.png"
+            alt="Maureen Mwangi"
+            style={{
+              maxWidth: "60%",// Limit image size
+              borderRadius: "10px",
+              boxShadow: "0px 4px 10px rgba(0,0,0,0.1)",
+            }}
+          />
         </div>
 
-        <About />
-        <Experience />
-        <Projects />
-        <Contact />
+        {/* About Section */}
+        <div
+          style={{
+            backgroundColor: "#ffffff",
+            padding: "30px 15px", // Reduced padding
+          }}
+        >
+          <About />
+        </div>
+
+        {/* Experience Section */}
+        <div
+          style={{
+            backgroundColor: "#f9f9f9",
+            padding: "30px 15px", // Reduced padding
+          }}
+        >
+          <Experience />
+        </div>
+
+        {/* Projects Section */}
+        <div
+          style={{
+            backgroundColor: "#eef7f6",
+            padding: "30px 15px", // Reduced padding
+          }}
+        >
+          <Projects />
+        </div>
+
+        {/* Contact Section */}
+        <div
+          style={{
+            backgroundColor: "#ffffff",
+            padding: "30px 15px", // Reduced padding
+          }}
+        >
+          <Contact />
+        </div>
       </Container>
 
       {/* Footer */}
